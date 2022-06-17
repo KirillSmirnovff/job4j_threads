465e5abf827ca8dd8fcf0fc28cd6b8b2e24b3ea6
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-06-17 13:57:01 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> { }
+        );
+        Thread second = new Thread(
+                () -> { }
+        );
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("first " + first.getState() + ", second " + second.getState());
+        }
+        System.out.println("first " + first.getState() + ", second " + second.getState());
+        System.out.println("The END");
+    }
+}
