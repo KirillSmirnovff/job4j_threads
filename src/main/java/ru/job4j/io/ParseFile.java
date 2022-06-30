4f606d004f4a4bfa1f6c892a6b963e423033db7b
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-06-30 14:46:48 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent(Predicate<Character> filter) {
+        String output = "";
+        try (BufferedReader i = new BufferedReader(new FileReader(file))) {
+            int data;
+            while ((data = i.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output += (char) data;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+
+
+}
---
+++ src/main/java/ru/job4j/io/SaveFile.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public final class SaveFile {
+    private final File file;
+
+    public SaveFile(File file) {
+        this.file = file;
+    }
+
+    public void saveContent(String content) {
+        try (BufferedWriter o = new BufferedWriter(new FileWriter(file))) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
