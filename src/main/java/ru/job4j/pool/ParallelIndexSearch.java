c6339c12b4ebd9b6598567e8378e8f4b881eb0f5
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-08-13 15:54:18 +0300

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pool/ParallelIndexSearch.java
@@ -0,0 +1,51 @@
+package ru.job4j.pool;
+
+import java.util.Objects;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
+
+    private final T value;
+    private final T[] array;
+    private final int from;
+    private final int to;
+
+    public ParallelIndexSearch(T value, T[] array, int from, int to) {
+        this.value = value;
+        this.array = array;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (to - from < 10) {
+            int result = -1;
+            for (int i = from; i <= to; i++) {
+                if (Objects.equals(array[i], value)) {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+        int mid = (to + from) / 2;
+        ParallelIndexSearch<T> leftSearch = new ParallelIndexSearch<>(value, array, from, mid);
+        ParallelIndexSearch<T> rightSearch = new ParallelIndexSearch<>(value,  array, mid + 1, to);
+        leftSearch.fork();
+        rightSearch.fork();
+        int left = leftSearch.join();
+        int right = rightSearch.join();
+        return Math.max(left, right);
+    }
+
+    public static void main(String[] args) {
+        Integer[] array = new Integer[100];
+        for (int i = 0; i < 100; i++) {
+            array[i] = i * 2;
+        }
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        System.out.println(forkJoinPool.invoke(new ParallelIndexSearch<Integer>(50, array, 0, array.length - 1)));
+    }
+}
